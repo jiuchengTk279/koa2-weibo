8018a7a64417a91b8de3ab72d32a0bdb7b047edd
jiucheng <15279232381@163.com>
2020-12-14 20:25:26 +0800

    抽离登录验证的中间件

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const router = require('koa-router')()
+const { loginRedirect, loginCheck } = require('../middlewares/loginCheck')
 
-router.get('/', async (ctx, next) => {
+
+router.get('/', loginRedirect, async (ctx, next) => {
   await ctx.render('index', {
     title: 'Hello Koa 2!',
     isMe: true,
@@ -29,7 +31,7 @@ router.get('/string', async (ctx, next) => {
   ctx.body = 'koa2 string'
 })
 
-router.get('/json', async (ctx, next) => {
+router.get('/json', loginCheck, async (ctx, next) => {
   const session = ctx.session
   if (session.viewNum == null) {
     session.viewNum = 0
