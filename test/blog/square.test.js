628ac1319e5d2bd0a13b4ac55f976ef6146711d3
jiucheng <15279232381@163.com>
2020-12-17 19:32:40 +0800

    微博广场的单元测试

---
+++ test/blog/square.test.js
@@ -0,0 +1,19 @@
+/**
+ * 广场 test
+ */
+
+const server = require('../server')
+const { COOKIE } = require('../testUserInfo')
+
+// 加载第一页数据
+test('广场，加载第一页数据', async () => {
+  const res = await server.get(`/api/square/loadMore/0`).set('cookie', COOKIE) // 设置 cookie
+  expect(res.body.errno).toBe(0)
+
+  const data = res.body.data
+  expect(data).toHaveProperty('isEmpty')
+  expect(data).toHaveProperty('blogList')
+  expect(data).toHaveProperty('pageSize')
+  expect(data).toHaveProperty('pageIndex')
+  expect(data).toHaveProperty('count')
+})
