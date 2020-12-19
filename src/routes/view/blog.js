167dabee05889775e9643399208a0a9fff54c956
jiucheng <15279232381@163.com>
2020-12-19 19:45:09 +0800

    关注和取消关注的接口

--- src/routes/view/blog.js
+++ src/routes/view/blog.js
@@ -55,6 +55,11 @@ router.get('/profile/:userName', loginRedirect, async (ctx, next) => {
   const fansResult = await getFans(curUserInfo.id)
   const { count: fansCount, fansList } = fansResult.data
 
+  // 我是否关注了此人
+  const amIFollowed = fansList.some(item => {
+    return item.userName === myUserName
+  })
+
   await ctx.render('profile', {
     blogData: {
       isEmpty,
@@ -69,7 +74,8 @@ router.get('/profile/:userName', loginRedirect, async (ctx, next) => {
       fansData: {
         count: fansCount,
         list: fansList
-      }
+      },
+      amIFollowed
     }
   })
 })
--- src/services/user-relation.js
+++ src/services/user-relation.js
@@ -38,6 +38,36 @@ async function getUsersByFollower (followerId) {
   }
 }
 
+/**
+ * 添加关注关系
+ * @param {number} userId 用户 id
+ * @param {number} followerId 被关注用户 id
+ */
+async function addFollower (userId, followerId) {
+  const result = await UserRelation.create({
+    userId,
+    followerId
+  })
+  return result.dataValues
+}
+
+/**
+ * 删除关注关系
+ * @param {number} userId 用户 Id
+ * @param {number} followerId 被关注用户 id
+ */
+async function deleteFollower (userId, followerId) {
+  const result = await UserRelation.delete({
+    where: {
+      userId,
+      followerId
+    }
+  })
+  return result > 0
+}
+
 module.exports = {
-  getUsersByFollower
+  getUsersByFollower,
+  addFollower,
+  deleteFollower
 }
