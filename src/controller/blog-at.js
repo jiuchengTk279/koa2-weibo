d41e923d5f481d42fe2aa51db8de4ebb863a9b20
jiucheng <15279232381@163.com>
2020-12-25 17:42:34 +0800

    开发 atMe 页面

--- src/controller/blog-at.js
+++ src/controller/blog-at.js
@@ -2,8 +2,9 @@
  * 微博 @ 关系 controller
  */
 
-const { getAtRelationCount } = require('../services/atRelation')
+const { getAtRelationCount, getAtUserBlogList } = require('../services/atRelation')
 const { SuccessModel, ErrorModel } = require('../model/ResModel')
+const { PAGE_SIZE } = require('../config/constant')
 
 /**
  * 获取 @ 我的微博数量
@@ -17,6 +18,32 @@ async function getAtMeCount (userId) {
   })
 }
 
+/**
+ * 获取 @ 用户的微博列表
+ * @param {number} userId user id
+ * @param {number} pageIndex page index
+ */
+async function getAtMeBlogList (userId, pageIndex = 0) {
+  // service
+  const result = await getAtUserBlogList({
+    userId,
+    pageIndex,
+    pageSize: PAGE_SIZE
+  })
+  const { count, blogList } = result
+
+  // 返回
+  return new SuccessModel({
+    isEmpty: blogList.length === 0,
+    blogList,
+    pageSize: PAGE_SIZE,
+    pageIndex,
+    count
+  })
+}
+
+
 module.exports = {
-  getAtMeCount
+  getAtMeCount,
+  getAtMeBlogList
 }
